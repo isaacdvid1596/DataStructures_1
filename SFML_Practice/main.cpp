ef2ca154c4e2a33839f987c7e018a9f3a42a0d48
isaacdvid1596 <44175229+isaacdvid1596@users.noreply.github.com>
2019-05-12 11:11:07 -0600

    Add files via upload

---
+++ SFML_Practice/main.cpp
@@ -0,0 +1,60 @@
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+
+int main()
+{
+
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial",sf::Style::Close | sf::Style::Resize);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	player.setFillColor(sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+				{
+			case sf::Event::Closed :
+				window.close();
+				break;
+			case sf::Event::Resized :
+				//std::cout <<"New Window Width : "<<evnt.size.width<<"New Window Height : "<< evnt.size.height << std::endl; 
+				printf("New Window Width: %i New Window Height : %i \n ", evnt.size.width, evnt.size.height);
+				break;	
+			case sf::Event::TextEntered :
+				if(evnt.text.unicode < 128)
+				printf("%c", evnt.text.unicode);
+				}
+
+
+			if (evnt.type == evnt.Closed)
+					{
+				window.close();
+					}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			player.move(-0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			player.move(0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			player.move(0.0f, -0.1f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			player.move(0.0f, 0.1f);
+		}
+		window.clear();
+		window.draw(player);
+		window.display();
+
+	}
+
+}
