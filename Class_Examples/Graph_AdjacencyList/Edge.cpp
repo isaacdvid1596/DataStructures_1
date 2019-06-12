7624f021e81f56acca967bdf38430170ae27d6f8
isaacdvid1596 <44175229+isaacdvid1596@users.noreply.github.com>
2019-06-12 00:29:40 -0600

    Add files via upload

---
+++ Class_Examples/Graph_AdjacencyList/Edge.cpp
@@ -0,0 +1,24 @@
+#include "Edge.h"
+
+
+
+Edge::Edge()
+{
+
+}
+
+
+Edge::~Edge()
+{
+
+}
+
+void Edge::setWeight(int weight)
+{
+	this->weight = weight;
+}
+
+int Edge::getWeight()
+{
+	return this->weight;
+}
---
+++ Class_Examples/Graph_AdjacencyList/Vertex.cpp
@@ -0,0 +1,24 @@
+#include "Vertex.h"
+
+
+
+Vertex::Vertex()
+{
+
+}
+
+
+Vertex::~Vertex()
+{
+
+}
+
+void Vertex::setName(string name)
+{
+	this->name = name;
+}
+
+string Vertex::getName()
+{
+	return this->name;
+}
